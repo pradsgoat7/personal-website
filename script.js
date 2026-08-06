27ac5f411c1f4abd62431654ea801c18f8863a23
Pradnyesh Patil <pradnyesh1611@gmail.com>
2026-08-06 13:44:23 +0530

    added a football to follow cursor movement

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    this.document.querySelector('.miniCircle').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+  })
+}
+
+circleMouseFollower();
